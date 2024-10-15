de7ab1c25716dfbc286150423b3fc2760e824b0c
bluetangerine0420 <a01051558311@gmail.com>
2024-10-15 22:05:53 +0900

    10 / 15

--- Assets/Scripts/Card/Cards/Drink/DrinkCard.cs
+++ Assets/Scripts/Card/Cards/Drink/DrinkCard.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class DrinkCard : Card
 {
-    public Equipment equipment;
+    public Drink drink;
 
     public override void UseCard()
     {
         if (BattleManager.battleManager.player1.curTurn)
         {
-            BattleManager.battleManager.player1.cur_Unit.GetEquipment(equipment);
+            BattleManager.battleManager.player1.cur_Unit.GetDrink(drink);
         }
         else if (BattleManager.battleManager.player2.curTurn)
         {
-            BattleManager.battleManager.player2.cur_Unit.GetEquipment(equipment);
+            BattleManager.battleManager.player2.cur_Unit.GetDrink(drink);
         }
     }
 }
--- Assets/Scripts/Card/Cards/Material/MaterialCard.cs
+++ Assets/Scripts/Card/Cards/Material/MaterialCard.cs
@@ -5,16 +5,17 @@ using UnityEngine;
 public class MaterialCard : Card
 {
     public Equipment equipment;
+    public Material material;
 
     public override void UseCard()
     {
         if (BattleManager.battleManager.player1.curTurn)
         {
-            BattleManager.battleManager.player1.cur_Unit.GetEquipment(equipment);
+            BattleManager.battleManager.player1.cur_Unit.GetMaterianl(material, equipment);
         }
         else if (BattleManager.battleManager.player2.curTurn)
         {
-            BattleManager.battleManager.player2.cur_Unit.GetEquipment(equipment);
+            BattleManager.battleManager.player2.cur_Unit.GetMaterianl(material, equipment);
         }
     }
 }
